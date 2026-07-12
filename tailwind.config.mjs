1e9e8d68f278bc6dbb0795bc0bae8e4a7ff9a8c3
Margaret Lazarovits <mlazarovits@gmail.com>
2026-07-12 18:13:25 -0500

    added skip boot button to skip animation (and cache skips too), uniformized formatting across sections, added founder cards and contact info

---
+++ tailwind.config.mjs
@@ -0,0 +1,33 @@
+export default {
+  theme: {
+    extend: {
+      colors: {
+	mainbg: "#191525",
+        cardbg: "#2B2438",
+        mainheading: "#F7F4EF",
+        secondheading: "#F4A6C6",
+        body: "#D3CDD9",
+	interactive: "#A77FD6",
+	accent1: "#8A4F6D",
+	accent2: "#E3BC68",
+	accent3: "#6E7A99",
+        terminal: "#B8FFD7",
+	cursor: "#D6FFE9",
+      },
+      backgroundImage: {
+	gradientbg: "linear-gradient(135deg, #191525 0%, #231B31 60%, #2A2438 100%)",
+      },
+      fontFamily: {
+	terminal: [
+	  "JetBrains mono",
+        ],
+        manrope: [
+          "Manrope",
+        ],
+        sora: [
+          "Sora"
+        ],
+      }
+    },
+  },
+}
